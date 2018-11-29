00f0cdf2c2fcd2da4a3586aa9d01a8cc2d066321
Matodor <matodor-zlo@yandex.ru>
2018-11-29 13:39:25 +0500

    new SaveableInt

---
+++ Scripts/mSaves/SaveableTypes/SaveableInt.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace mFramework.Saves
+{
+    public class SaveableInt : SaveableValue<int>
+    {
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+
+        public override byte[] Serialize()
+        {
+            return new[]
+            {
+                (byte) Value,
+                (byte) (Value >> 8),
+                (byte) (Value >> 16),
+                (byte) (Value >> 24),
+            };
+        }
+
+        public override bool Deserialize(byte[] array, int startIndex)
+        {
+            if (array == null || array.Length == 0 || startIndex + 4 > array.Length)
+                return false;
+
+            Value = array[startIndex + 0] |
+                    array[startIndex + 1] << 8 |
+                    array[startIndex + 2] << 16 |
+                    array[startIndex + 3] << 24;
+            return true;
+        }
+    }
+}
